4403827a424104ba06fd9d1466b0bfc2fb60a5f9
LEONHEIL <leozcordz@gmail.com>
2025-10-04 13:05:31 +0800

    Created axios api calls ready for frontend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -7,17 +7,50 @@ export const fetchItems = async () => {
     const { data } = await axios.get(`${API_URL}/inventory`);
     return data;
   } catch (error) {
-    console.error('Error fetching data', error);
+    console.error('Error fetching data', error.message);
     throw error;
   }
 };
 
-export const addItem = async (item) => {
+export const fetchItemsById = async (id) => {
   try {
-    const { data } = await axios.post(`${API_URL}/inventory`, item);
+    const { data } = await axios.get(`${API_URL}/inventory/${id}`);
     return data;
   } catch (error) {
-    console.error('Error adding item', error);
+    console.error('Error fetching data', error.message);
+    throw error;
+  }
+};
+
+export const createItem = async (newitem) => {
+  try {
+    const { data } = await axios.post(`${API_URL}/inventory`, newitem);
+    return data;
+  } catch (error) {
+    console.error('Error creating item', error.message);
+    throw error;
+  }
+};
+
+export const updateItem = async (id, updatedItem) => {
+  try {
+    const { data } = await axios.patch(
+      `${API_URL}/inventory/${id}`,
+      updatedItem
+    );
+    return data;
+  } catch (error) {
+    console.error('Error creating item', error.message);
+    throw error;
+  }
+};
+
+export const deleteItem = async (id) => {
+  try {
+    const { data } = await axios.delete(`${API_URL}/inventory/${id}`);
+    return data;
+  } catch (error) {
+    console.error('Error creating item', error.message);
     throw error;
   }
 };
