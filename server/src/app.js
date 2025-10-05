402f318125d95cdb5a0be1906a8599e5038cdcfb
LEONHEIL <leozcordz@gmail.com>
2025-10-05 10:39:57 +0800

    Modularize backend: split server and app into separate files

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import inventoryRoute from './routes/inventoryRoute.js';
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: 'http://localhost:5137',
+  })
+);
+
+// Routes
+app.use('/', inventoryRoute);
+
+export default app;
